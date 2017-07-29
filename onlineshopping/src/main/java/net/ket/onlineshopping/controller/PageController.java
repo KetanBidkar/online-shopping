d9831888c13bb10d4c1b38a6dc4e1bfbdaa4acaf
KetanBidkar <bidkarketan@gmail.com>
2017-07-30 02:19:06 +0530

    footer and new theme and some minor issue resolved

--- onlineshopping/src/main/java/net/ket/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/ket/onlineshopping/controller/PageController.java
@@ -10,9 +10,29 @@ public class PageController {
 	@RequestMapping(value={"/","/index","/home"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");  //logical name of view= pages
-		mv.addObject("greeting","Welcome to spring MVC");//for fetching data to view using key greeting
+//		mv.addObject("greeting","Welcome to spring MVC");//for fetching data to view using key greeting
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
+	
+	@RequestMapping(value={"/about"})
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");  //logical name of view= pages
+		mv.addObject("title","About");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");  //logical name of view= pages
+		mv.addObject("title","Contact");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	
+
 	
 //	@RequestMapping(value="/test")
 //	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting) //variable of string type paased thru add object
